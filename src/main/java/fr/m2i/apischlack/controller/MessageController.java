df1a9f103abff3cc6f60b1632e32ef34caf196c0
benjamin perrin <perrin.benjamin77@gmail.com>
2022-09-27 00:28:34 +0200

    add CrossOrigin

--- src/main/java/fr/m2i/apischlack/controller/MessageController.java
+++ src/main/java/fr/m2i/apischlack/controller/MessageController.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author ben
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/v1/messages")
 public class MessageController {
